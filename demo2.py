0290f333aad1cfc3d60652a261fc5d31695e4a5b
Lreii <pasambakenjoshua@gmail.com>
2025-12-12 16:59:27 +0800

    Add files via upload

---
+++ demo2.py
@@ -0,0 +1,18 @@
+def greeter(name):  #own function number 1
+    print(f"Hello {name}, How are you? ")
+
+def summ(x):    #own function number 2
+    total = 0
+    for y in range(x, 0, -1):
+        total += y  #all the number counted will go to total 
+    print(f"The of {x} is {total}") #print the total or sum of the numbers
+greeter("ewrqwe")
+greeter("ewrw")
+greeter("ewwerwerqwe")
+greeter("ewrqwerwerwerwe")
+
+
+summ(15)
+summ(3)
+summ(5)
+summ(12)
